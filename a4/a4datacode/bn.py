9f0256552191982f9a4e1d45571e1a6024d86249
Andy Zhang <andzhng@gmail.com>
2018-03-25 15:33:22 -0400

    a4

---
+++ a4/a4datacode/bn.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+
+def fields_view(array, fields):
+    return array.getfield(np.dtype(
+        {name: array.dtype.fields[name] for name in fields}
+    ))
+
+
+def remove_field_name(a, names):
+    """Removes a field name of a structuredarray `a`."""
+    if type(names) == str:
+        names = [names]
+    new_names = list(a.dtype.names)
+    for n in names:
+        if n in new_names:
+            new_names.remove(n)
+    b = a[new_names]
+    return b
+
+
+def to_structured_array(a, names):
+    return np.core.records.fromarrays(a.transpose(),
+                                      names=names)
+
+
+def unique(l):
+    output = set()
+    for x in l:
+        output.add(x)
+    return list(output)
+
+
+def print_factors(factorList):
+    for f in factorList:
+        print f.dtype.names, f.view((float, len(f.dtype.names)))
+
+
+def restrict(factor, variable, value):
+    """Factor out rows where variable is not value, and then remove variable column.
+
+    factor is a structuredarray
+    """
+    if variable not in factor.dtype.names:
+        return factor
+    indices = np.where(factor[variable] != value)
+    result = np.delete(factor, indices, axis=0)  # Delete rows
+    temp = remove_field_name(result, variable)
+    return to_structured_array(np.array(temp.tolist()), ','.join(temp.dtype.names))
+
+
+def multiply(factor1, factor2):
+    if len(factor1.dtype.names) == 1:
+        factor2['val'] *= factor1[factor1.dtype.names[0]][0]
+        return factor2
+    elif len(factor2.dtype.names) == 1:
+        factor1['val'] *= factor2[factor2.dtype.names[0]][0]
+        return factor1
+
+    names = set(factor1.dtype.names).intersection(set(factor2.dtype.names))
+    names.remove('val')  # Don't need 'val'
+
+    # Let's assume that names is only 1 value now
+    col = list(names)
+    vals = fields_view(factor2, list(col))
+
+    # Merge columns
+    f1_names = list(factor1.dtype.names)
+    f2_names = list(factor2.dtype.names)
+    for name in col:
+        f1_names.remove(name)
+        f2_names.remove(name)
+    f1_names.remove('val')
+    f2_names.remove('val')
+    new_col = f1_names + col + f2_names + ['val']
+
+    temp1 = factor1
+    temp2 = factor2
+    for name in col:
+        temp1 = remove_field_name(temp1, name)
+        temp2 = remove_field_name(temp2, name)
+
+    rows = []
+    for v in vals:
+        row1 = []
+        row2 = []
+        for i, c in enumerate(col):
+            row1.append(np.array(np.where(factor1[c] == v[i])[0]))
+            row2.append(np.array(np.where(factor2[c] == v[i])[0]))
+        row1 = reduce(np.intersect1d, np.array(row1))
+        row2 = reduce(np.intersect1d, np.array(row2))
+
+        for i in row1:
+            for j in row2:
+                t1 = list(temp1[i])
+                t2 = list(temp2[j])
+                # Each permutation for each column where col == v
+                rows.append(t1[0:-1] + list(v) + t2[0:-1] + [t1[-1] * t2[-1]])
+
+    result = to_structured_array(np.array(rows), new_col)
+    return result
+
+
+def sumout(factor, variable):
+    f = remove_field_name(factor, ['val', variable])
+    u, indices = np.unique(f, return_inverse=True, axis=0)
+    indices = np.array(indices)
+
+    rows = []
+    for i in range(0, len(u)):
+        row = list(u[i]) + [0]
+        for j in np.where(indices == i)[0]:
+            row[-1] += factor[j][-1]
+        rows.append(row)
+
+    # Get new column names
+    new_cols = list(factor.dtype.names)
+    new_cols.remove(variable)
+
+    return to_structured_array(np.array(rows), new_cols)
+
+
+def normalize(factor):
+    s = np.sum(factor['val'])
+    factor['val'] /= s
+    return factor
+
+
+def inference(factorList, queryVariables, orderedListOfHiddenVariables, evidenceList):
+    """
+    evidenceList: dict where key is variable name and value is the value.
+    """
+    for var in evidenceList:
+        for i, factor in enumerate(factorList):
+            factorList[i] = restrict(factor, var, evidenceList[var])
+
+    # print_factors(factorList)
+
+    for var in orderedListOfHiddenVariables:
+        to_multiply = []
+        for i, factor in reversed(list(enumerate(factorList))):
+            if var in factor.dtype.names:
+                f = factorList.pop(i)
+                to_multiply.append(f)
+
+        product = to_multiply[0]
+        for i in range(1, len(to_multiply)):
+            product = multiply(product, to_multiply[i])
+
+        f = sumout(product, var)
+        factorList.append(f)
+
+    # print_factors(factorList)
+    product = factorList[0]
+    for i in range(1, len(factorList)):
+        product = multiply(product, factorList[i])
+
+    return normalize(product)
--- a4/a4datacode/em.py
+++ a4/a4datacode/em.py
@@ -1,3 +1,11 @@
+from bn import *
+
+NOT_RECORDED = -1
+NOT_PRESENT = 0
+MILD = 1
+SEVERE = 2
+
+
 class Case:
 
     def __init__(self, sloe, fori, dega, trimo, dunn):
@@ -7,18 +15,89 @@ class Case:
         self.trimono = trimo
         self.dunnetts = dunn
 
+    @property
+    def s(self):
+        return self.sloepnea
+
+    @property
+    def f(self):
+        return self.foriennditis
+
+    @property
+    def d(self):
+        return self.degar_spots
+
+    @property
+    def t(self):
+        return self.trimono
+
+    @property
+    def dunnetts(self):
+        return self.dunnetts
+
 
 def load_data(filename):
+    data = []
     with open(filename, 'r') as f:
         for l in f.readlines():
             sloe, fori, dega, trimo, dunn = l.strip().split(' ')
             case = Case(sloe, fori, dega, trimo, dunn)
-            print case
+            data.append(case)
+
+    return data
 
 
 def main():
-    load_data('traindata.txt')
+    print load_data('traindata.txt')
 
+
+# Pr(Dunnetts)
+f0 = to_structured_array(np.array([['absent',.5],
+                                  ['mild',.25],
+                                  ['severe',.25]]), 'dunnetts,val')
+
+# Pr(Trimono)
+f1 = to_structured_array(np.array([[0,.9], [1,.1]]), 'trimono,val')
+
+# Pr(foriennditis|dunnetts)
+f2 = to_structured_array(np.array([['absent',0,.99],
+                                   ['absent',1,.01],
+                                   ['mild',0,.7],
+                                   ['mild',1,.3],
+                                   ['severe',0,.2],
+                                   ['severe',1,.8]]),
+                         'dunnetts,foriennditis,val')
+
+# Pr(foriennditis|dunnetts)
+f3 = to_structured_array(np.array([['absent',0,.99],
+                                   ['absent',1,.01],
+                                   ['mild',0,.3],
+                                   ['mild',1,.8],
+                                   ['severe',0,.7],
+                                   ['severe',1,.3]]),
+                         'dunnetts,degar,val')
+
+# Pr(foriennditis|dunnetts)
+f4 = to_structured_array(np.array([['absent',0,0,.99],
+                                   ['absent',0,1,.01],
+                                   ['absent',1,0,.999],
+                                   ['absent',1,1,.001],
+                                   ['mild',0,0,.5],
+                                   ['mild',0,1,.5],
+                                   ['mild',1,0,.95],
+                                   ['mild',1,1,.05],
+                                   ['severe',0,0,.5],
+                                   ['severe',0,1,.5],
+                                   ['severe',1,0,.95],
+                                   ['severe',1,1,.05]]),
+                         'dunnetts,trimono,sloepnea,val')
+
+
+# Pr(Fraud|Trav)
+f5 = to_structured_array(np.array([[1,0,.99], [1,1,.01], [0,1,.004], [0,0,.996]]), 'Trav,Fraud,val')
+
+# Pr(Fraud|Trav)
+f6 = to_structured_array(np.array([[1,0,.99], [1,1,.01], [0,1,.004], [0,0,.996]]), 'Trav,Fraud,val')
 
 if __name__ == '__main__':
     main()
